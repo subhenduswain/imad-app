7660d0d13e2d8b84499b4456b4793cba9715004b
subhenduswain <36529878+subhenduswain@users.noreply.github.com>
2018-03-12 17:11:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ var element = document.getElementById('madi');
 var pos = 0;
 
 function moveright(){
-    pos = pos + 2;
+    pos = pos + 1;
     element.style.marginLeft = pos + 'px';
 }
 element.onclick = function(){
-    var interval = setInterval(moveright,100);
+    var interval = setInterval(moveright,50);
 }
