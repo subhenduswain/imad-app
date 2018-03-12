ae86d746177b215e8be01d443c487382499b7c90
subhenduswain <36529878+subhenduswain@users.noreply.github.com>
2018-03-12 17:05:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var img = document.getElementById('madi');
+
+var pos = 0;
+
+function moveright(){
+    pos += 2;
+    img.style.marginLeft = pos + 'px';
+}
+
+var id = setInterval(100,moveright);
